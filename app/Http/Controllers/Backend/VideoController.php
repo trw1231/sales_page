e55d3e26d43d1041b5a53398d75d37805293d270
bozz <trw1231@gmail.com>
2021-02-26 18:34:38 +0700

    change url

--- app/Http/Controllers/Backend/VideoController.php
+++ app/Http/Controllers/Backend/VideoController.php
@@ -40,14 +40,43 @@ class VideoController extends Controller
         $request->validate([
             'video'=>'required',
         ]);
-
-       
+       //This is a general function for generating an embed link of an FB/Vimeo/Youtube Video.
+       $url = $request->video;
+    $finalUrl = '';
+    if(strpos($url, 'facebook.com/') !== false) {
+        //it is FB video
+        $finalUrl.='https://www.facebook.com/plugins/video.php?href='.rawurlencode($url).'&show_text=1&width=200';
+    }else if(strpos($url, 'vimeo.com/') !== false) {
+        //it is Vimeo video
+        $videoId = explode("vimeo.com/",$url)[1];
+        if(strpos($videoId, '&') !== false){
+            $videoId = explode("&",$videoId)[0];
+        }
+        $finalUrl.='https://player.vimeo.com/video/'.$videoId;
+    }else if(strpos($url, 'youtube.com/') !== false) {
+        //it is Youtube video
+        $videoId = explode("v=",$url)[1];
+        if(strpos($videoId, '&') !== false){
+            $videoId = explode("&",$videoId)[0];
+        }
+        $finalUrl.='https://www.youtube.com/embed/'.$videoId;
+    }else if(strpos($url, 'youtu.be/') !== false){
+        //it is Youtube video
+        $videoId = explode("youtu.be/",$url)[1];
+        if(strpos($videoId, '&') !== false){
+            $videoId = explode("&",$videoId)[0];
+        }
+        $finalUrl.='https://www.youtube.com/embed/'.$videoId;
+    }else{
+        //Enter valid video URL
+    }
+    
         $max_sort = Category_image::where('category_sale_id',$id)->max('sort');
 
         Category_image::create([
             'category_sale_id' => $id,
             'content_type' => 'video',
-            'content' => $request->video,
+            'content' => $finalUrl,
             'sort' => $max_sort+1,
 
         ]);
